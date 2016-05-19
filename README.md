1de69bddcce805fa8a78380243a305a18f261c29
Matt Sprague <matt@sourcegraph.com>
2016-05-19 16:29:43 -0700

    and then there was sourcegraph edge for sublime

---
+++ README.md
@@ -0,0 +1,109 @@
+# Sourcegraph Edge Sublime Plugin [![CircleCI](https://circleci.com/gh/luttig/sg-sublime.svg?style=svg)](https://circleci.com/gh/luttig/sg-sublime)
+
+## Overview
+
+The Sourcegraph Sublime Plugin allows you to view Go definitions in real-time on [sourcegraph.com](http://www.sourcegraph.com) as you code, so you can stay focused on what's important: your code. When your cursor is on a certain Go symbol, it should load in an Edge channel in your browser:
+
+![Sourcegraph Edge Sublime Plugin](images/setup.jpg)
+
+## Setup
+
+To install Sourcegraph Edge, clone the `sg-sublime` plugin into your Sublime Text 3 Package folder:
+
+OSX:
+
+```shell
+git clone https://github.com/luttig/sg-sublime ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/sg-sublime
+```
+
+Linux:
+
+```shell
+git clone https://github.com/luttig/sg-sublime ~/.config/sublime-text-3/Packages/sg-sublime
+```
+
+## Usage
+
+The plugin should automatically open a browser tab to initialize your Edge session when in Go files. If, for any reason, your tab gets closed, press <kbd>&#8984;</kbd>+<kbd>.</kbd>, <kbd>&#8984;</kbd>+<kbd>0</kbd> in any file to reopen the Sourcegraph Edge tab. Alternatively, you can click on `Sublime Text > Preferences > Package Settings > Sourcegraph > Reopen Browser Tab`. As you navigate through Go files, your browser should automatically load the definition and references for recognized symbols.
+
+## Flags
+
+Sourcegraph Edge has a number of flags to customize the plugin for you. To change your plugin settings, open `Sourcegraph.sublime-settings` by clicking `Sublime Text > Preferences > Package Settings > Sourcegraph > Settings - User`.
+
+### Custom GOBIN or GOPATH
+
+The plugin assumes a `GOBIN` of `/usr/local/bin/go`, and loads your `/bin/bash` startup scripts to search for the `GOPATH` environment variable. If your `GOPATH` is not defined in your startup scripts, the plugin then searches for GoSublime settings files for `GOPATH`. If the plugin cannot find your `GOPATH`, or if you would like to use a custom `GOPATH` or `GOBIN`, add them in the settings file as follows:
+
+```yml
+{
+	...
+	"GOPATH": "/path/to/gopath",
+	"GOBIN": "/path/to/gobin",
+	...
+}
+```
+
+### Auto-process selection
+
+Sourcegraph automatically processes your code as you type. If you want to disable this feature, set the `AUTO_PROCESS` flag to `false`. If you disable it, you must press <kbd>&#8984;</kbd>+<kbd>,</kbd>, <kbd>&#8984;</kbd>+<kbd>0</kbd> to process your current selection.
+
+```yml
+{
+	...
+	"AUTO_PROCESS": false,
+	...
+}
+```
+
+### Auto-open browser
+
+This setting auto-opens the channel in your browser when you click on a definition in Go. If it is set to false, then you can access the edge channel by clicking <kbd>&#8984;</kbd>+<kbd>.</kbd>, <kbd>&#8984;</kbd>+<kbd>0</kbd>. To disable auto-opening, set the `AUTO_OPEN` flag to `false` in your settings:
+
+```yml
+{
+	...
+	"AUTO_OPEN": false,
+	...
+}
+```
+
+### Verbose logging
+
+This setting gives verbose output from the Sourcegraph Edge plugin to the Sublime Text console, which can be helping when troubleshooting Sourcegraph Edge. To open the Sublime console, simply type <kbd>&#159;</kbd>+<kbd>`</kbd>. Different levels of logging are available:
+
+No logging: 0
+Only log symbols identified by godefinfo: 1
+Log network calls: 2
+Log all debugging information: 3
+
+```yml
+{
+	...
+	"LOG_LEVEL": 1,
+	...
+}
+```
+
+## Godefinfo
+
+Sourcegraph Edge should automatically install `godefinfo` when it loads your settings. However, if you still receive error message about `godefinfo` installation, you can install it manually by running the following command:
+
+```shell
+go get -u github.com/sqs/godefinfo
+```
+
+### Local server
+
+If you want to try the plugin on a local Sourcegraph server, you can define its base URL in this file using the key `SG_BASE_URL`.
+
+```yml
+{
+	...
+	"SG_BASE_URL": "https://www.sourcegraph.com",
+	...
+}
+```
+
+## Support
+
+The Sourcegraph Edge Sublime Plugin has currently only been tested using Sublime Text 3 on Mac OS X and Linux.
---
+++ sg_sublime.py
@@ -0,0 +1,121 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+import sg_lib
+
+import sublime
+
+import sublime_plugin
+
+SETTINGS_FILENAME = 'Sourcegraph.sublime-settings'
+GOSUBLIME_SETTINGS_FILENAME = 'GoSublime.sublime-settings'
+SUBLIME_VERSION = int(sublime.version())
+
+SG_LIB_INSTANCE = {}
+
+
+def find_gopath_from_gosublime():
+	if sublime.load_settings(GOSUBLIME_SETTINGS_FILENAME).has('env'):
+		gosubl_env = sublime.load_settings(GOSUBLIME_SETTINGS_FILENAME).get('env')
+		if 'GOPATH' in gosubl_env:
+			return gosubl_env['GOPATH'].replace('$HOME', os.environ.get('HOME')).replace(':$GS_GOPATH', '')
+	return None
+
+
+def load_settings(settings):
+	sgedge_settings = sg_lib.Settings()
+
+	if settings.has('LOG_LEVEL'):
+		sg_lib.LOG_LEVEL = settings.get('LOG_LEVEL')
+		sg_lib.log_output('[settings] Found logging setting in Settings file: %s' % sg_lib.LOG_LEVEL)
+	if settings.has('ENABLE_LOOKBACK'):
+		sgedge_settings.ENABLE_LOOKBACK = settings.get('ENABLE_LOOKBACK')
+	if settings.has('SG_BASE_URL'):
+		sgedge_settings.SG_BASE_URL = settings.get('SG_BASE_URL').rstrip('/')
+	if settings.has('SG_LOG_FILE'):
+		sg_lib.SG_LOG_FILE = settings.get('SG_LOG_FILE')
+	if settings.has('AUTO_OPEN'):
+		sgedge_settings.AUTO_OPEN = settings.get('AUTO_OPEN')
+	if settings.has('AUTO_PROCESS'):
+		sgedge_settings.AUTO_PROCESS = settings.get('AUTO_PROCESS')
+	if settings.has('GOBIN'):
+		sgedge_settings.GOBIN = settings.get('GOBINGOT').rstrip(os.sep)
+
+	shell_gopath, err, return_code = sg_lib.run_shell_command(sgedge_settings.ENV.get('SHELL'), "echo ${GOPATH}")
+	if settings.has('GOPATH'):
+		sgedge_settings.ENV['GOPATH'] = str(settings.get('GOPATH').rstrip(os.sep)).strip()
+		sg_lib.log_output('[settings] Using GOPATH found in Sublime settings file: %s' % sgedge_settings.ENV['GOPATH'])
+	elif shell_gopath and shell_gopath.rstrip(os.sep).strip() != '':
+		sgedge_settings.ENV['GOPATH'] = shell_gopath.rstrip(os.sep).strip()
+	elif find_gopath_from_gosublime():
+		sgedge_settings.ENV['GOPATH'] = find_gopath_from_gosublime()
+		sg_lib.log_output('[settings] Found GOPATH in GoSublime settings: %s' % sgedge_settings.ENV['GOPATH'])
+	if sgedge_settings.ENV['GOPATH']:
+		sgedge_settings.ENV['GOPATH'] = sgedge_settings.ENV['GOPATH'].replace('~', os.environ.get('HOME'))
+		print(sgedge_settings.ENV['GOPATH'])
+	global SG_LIB_INSTANCE
+	SG_LIB_INSTANCE = sg_lib.SourcegraphEdge(sgedge_settings)
+	SG_LIB_INSTANCE.post_load()
+
+
+def reload_settings():
+	old_base_url = SG_LIB_INSTANCE.settings.SG_BASE_URL
+	settings = sublime.load_settings(SETTINGS_FILENAME)
+	load_settings(settings)
+	if SG_LIB_INSTANCE.settings.SG_BASE_URL != old_base_url and SG_LIB_INSTANCE.settings.AUTO_OPEN:
+		SG_LIB_INSTANCE.open_edge_channel()
+
+
+def plugin_loaded():
+	settings = sublime.load_settings(SETTINGS_FILENAME)
+	settings.clear_on_change('sg-reload')
+	settings.add_on_change('sg-reload', reload_settings)
+
+	gosublime_settings = sublime.load_settings(GOSUBLIME_SETTINGS_FILENAME)
+	gosublime_settings.clear_on_change('sg-reload-gosubl')
+	gosublime_settings.add_on_change('sg-reload-gosubl', reload_settings)
+
+	load_settings(settings)
+
+
+def cursor_offset(view):
+	row, col = view.rowcol(view.sel()[0].begin())
+	symbol_start_offset = sg_lib.search_for_symbols(view.sel()[0].begin(), view.substr(view.line(view.sel()[0])), row, col, SG_LIB_INSTANCE.settings.ENABLE_LOOKBACK)
+
+	string_before = view.substr(sublime.Region(0, symbol_start_offset))
+	string_before.encode('utf-8')
+	buffer_before = bytearray(string_before, encoding='utf8')
+	return str(len(buffer_before))
+
+
+def process_selection(view):
+	if len(view.sel()) == 0:
+		return
+	args = sg_lib.LookupArgs(filename=view.file_name(), cursor_offset=cursor_offset(view), preceding_selection=str.encode(view.substr(sublime.Region(0, view.size()))), selected_token=view.substr(view.extract_scope(view.sel()[0].begin())))
+	SG_LIB_INSTANCE.on_selection_modified_handler(args)
+
+
+class SgOpenEdgeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		SG_LIB_INSTANCE.open_edge_channel(hard_refresh=True)
+		process_selection(self.view)
+
+
+class SgOpenLogCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		self.window.open_file(sg_lib.SG_LOG_FILE)
+
+
+class SgManualProcessCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		process_selection(self.view)
+
+
+class SgAutoProcessCommand(sublime_plugin.EventListener):
+	def __init__(self):
+		super().__init__()
+
+	def on_selection_modified_async(self, view):
+		if SG_LIB_INSTANCE.settings.AUTO_PROCESS:
+			process_selection(view)
